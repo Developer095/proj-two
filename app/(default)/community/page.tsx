cb083652ff9741c38c6bfdbacf35f3a577df826d
NullAlgorithm <abdullahtoheed321@gmail.com>
2024-11-06 23:27:08 +0500

    communtiy page underdeveloping

---
+++ app/(default)/community/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Community = () => {
+  return (
+    <div>
+      Community
+      <div>
+        <div>hello</div>
+      </div>
+    </div>
+  );
+};
+
+export default Community;
