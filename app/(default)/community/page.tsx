9d82c418ba86c48fc2387d55dcf2a1ea15d99fdf
NullAlgorithm <abdullahtoheed321@gmail.com>
2024-11-08 00:35:49 +0500

    community page complete

--- app/(default)/community/page.tsx
+++ app/(default)/community/page.tsx
@@ -1,11 +1,191 @@
 import React from "react";
-
+import image from "biyani.jpg";
 const Community = () => {
   return (
-    <div>
-      Community
-      <div>
-        <div>hello</div>
+    <div className="scrollTop:100 bg-[#020817] pb-20">
+      <div className="">
+        <div className="mb-[80px] ml-14 mt-32 text-[25px] font-bold uppercase">
+          All Users
+        </div>
+      </div>
+
+      <div className="ml-6 mr-6 grid gap-4 sm:grid-cols-1 lg:grid-cols-3">
+        <div className="h-[290px] w-full rounded-[12px] border-2 border-[#1e293b] bg-[#151821]">
+          <div className="mt-4 flex justify-center">
+            <img
+              src="./image/a.png"
+              alt="image"
+              className="h-24 w-[100px] rounded-[50px]"
+            />
+          </div>
+          <span className="mt-4 block flex justify-center">User_1081</span>
+          {/* <span className="mt-2 block flex justify-center text-[#9ca3af]">
+            @asil0312
+          </span> */}
+          <span className="mt-2 block flex justify-center text-[#9ca3af]">
+            Data Scientist
+          </span>
+          <span className="mt-2 flex pl-4 pr-4 text-center text-sm text-[#B2CCD5]">
+            To extract insights from data, use techniques like machine learning
+            and statistical analysis.
+          </span>
+        </div>
+
+        <div className="h-[290px] w-full rounded-[12px] border-2 border-[#1e293b] bg-[#151821]">
+          <div className="mt-4 flex justify-center">
+            <img
+              src="./image/b.png"
+              alt="image"
+              className="h-24 w-[100px] rounded-[50px]"
+            />
+          </div>
+          <span className="mt-4 block flex justify-center">User_574</span>
+          <span className="mt-2block flex justify-center text-[#9ca3af]">
+            Cybersecurity Analyst
+          </span>
+          <span className="mt-4 flex pl-4 pr-4 text-center text-sm text-[#B2CCD5]">
+            To protect systems from cyber threats, use security tools and best
+            practices.
+          </span>
+        </div>
+
+        <div className="h-[290px] w-full rounded-[12px] border-2 border-[#1e293b] bg-[#151821]">
+          <div className="mt-4 flex justify-center">
+            <img
+              src="./image/j.jpg"
+              alt="image"
+              className="h-24 w-[100px] rounded-[50px]"
+            />
+          </div>
+          <span className="mt-4 block flex justify-center">User_3736</span>
+          <span className="mt-2 block flex justify-center text-[#9ca3af]">
+            Software Engineer
+          </span>
+          <span className="mt-4 flex pl-4 pr-4 text-center text-sm text-[#B2CCD5]">
+            To develop software solutions, use programming languages like Python
+            and Java.
+          </span>
+        </div>
+
+        <div className="h-[290px] w-full rounded-[12px] border-2 border-[#1e293b] bg-[#151821]">
+          <div className="mt-4 flex justify-center">
+            <img
+              src="./image/s.png"
+              alt="image"
+              className="h-24 w-[100px] rounded-[50px]"
+            />
+          </div>
+          <span className="mt-4 block flex justify-center">User_2345</span>
+          <span className="mt-2 block flex justify-center text-[#9ca3af]">
+            Database Administrator
+          </span>
+          <span className="mt-4 flex pl-4 pr-4 text-center text-sm text-[#B2CCD5]">
+            To manage databases, use SQL and NoSQL technologies.
+          </span>
+        </div>
+
+        <div className="h-[290px] w-full rounded-[12px] border-2 border-[#1e293b] bg-[#151821]">
+          <div className="mt-4 flex justify-center">
+            <img
+              src="./image/f.png"
+              alt="image"
+              className="h-24 w-[100px] rounded-[50px]"
+            />
+          </div>
+          <span className="mt-4 block flex justify-center">User_2352</span>
+          <span className="mt-2 block flex justify-center text-[#9ca3af]">
+            UI/UX Designer
+          </span>
+          <span className="mt-4 flex pl-4 pr-4 text-center text-sm text-[#B2CCD5]">
+            To create user-friendly interfaces, use design tools like Figma and
+            Adobe XD.
+          </span>
+        </div>
+
+        <div className="h-[290px] w-full rounded-[12px] border-2 border-[#1e293b] bg-[#151821]">
+          <div className="mt-4 flex justify-center">
+            <img
+              src="./image/r.jpg"
+              alt="image"
+              className="h-24 w-[100px] rounded-[50px]"
+            />
+          </div>
+          <span className="mt-4 block flex justify-center">User_6777</span>
+          <span className="mt-2 block flex justify-center text-[#9ca3af]">
+            Network Engineer
+          </span>
+          <span className="mt-4 flex pl-4 pr-4 text-center text-sm text-[#B2CCD5]">
+            To design and maintain networks, use protocols like TCP/IP and
+            routing protocols.
+          </span>
+        </div>
+
+        <div className="h-[290px] w-full rounded-[12px] border-2 border-[#1e293b] bg-[#151821]">
+          <div className="mt-4 flex justify-center">
+            <img
+              src="./image/h.png"
+              alt="image"
+              className="h-24 w-[100px] rounded-[50px]"
+            />
+          </div>
+          <span className="mt-4 block flex justify-center">User_7654</span>
+          <span className="mt-2 block flex justify-center text-[#9ca3af]">
+            DevOps Engineer
+          </span>
+          <span className="mt-4 flex pl-4 pr-4 text-center text-sm text-[#B2CCD5]">
+            To automate software delivery, use tools like Jenkins and Docker.
+          </span>
+        </div>
+
+        <div className="h-[290px] w-full rounded-[12px] border-2 border-[#1e293b] bg-[#151821]">
+          <div className="mt-4 flex justify-center">
+            <img
+              src="./image/g.png"
+              alt="image"
+              className="h-24 w-[100px] rounded-[50px]"
+            />
+          </div>
+          <span className="mt-4 block flex justify-center">User_6485</span>
+          <span className="mt-2 block flex justify-center text-[#9ca3af]">
+            Game Designer
+          </span>
+          <span className="mt-4 flex pl-4 pr-4 text-center text-sm text-[#B2CCD5]">
+            To create immersive game worlds, use game engines like Unity and
+            Unreal Engine.
+          </span>
+        </div>
+
+        <div className="h-[290px] w-full rounded-[12px] border-2 border-[#1e293b] bg-[#151821]">
+          <div className="mt-4 flex justify-center">
+            <img
+              src="./image/b.png"
+              alt="image"
+              className="h-24 w-[100px] rounded-[50px]"
+            />
+          </div>
+          <span className="mt-4 block flex justify-center">User_2844</span>
+          {/* <span className="mt-2 block flex justify-center text-[#9ca3af]">
+            @asil0312
+          </span> */}
+          <span className="mt-2 block flex justify-center text-[#9ca3af]">
+            Software Developer
+          </span>
+          <span className="mt-4 flex pl-4 pr-4 text-center text-sm text-[#B2CCD5]">
+            To center, use display: flex, justify-content: center, and
+            align-items: center.
+          </span>
+        </div>
+      </div>
+      <div className="mt-12 flex justify-center">
+        <button className="h-10 w-14 rounded-[4px] border-none bg-gray-700 text-black">
+          Prev
+        </button>
+        <button className="ml-2 mr-2 h-10 w-14 rounded-[4px] border-none bg-orange-400">
+          1
+        </button>
+        <button className="h-10 w-14 rounded-[4px] border-none bg-gray-200 text-black">
+          Next
+        </button>
       </div>
     </div>
   );
