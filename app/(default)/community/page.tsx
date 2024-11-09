b243e6d599360b4cc3c62a6f8ee9bc5f33918560
NullAlgorithm <abdullahtoheed321@gmail.com>
2024-11-09 23:59:08 +0500

    updaded community code with map function
    the user data is static , but the the integration of API then will will has be change accordingly

--- app/(default)/community/page.tsx
+++ app/(default)/community/page.tsx
@@ -1,180 +1,134 @@
+//   <div className="ml-6 mr-6 grid gap-4 sm:grid-cols-1 lg:grid-cols-3">
+//     <div className="h-[290px] w-full rounded-[12px] border-2 border-[#1e293b] bg-[#151821]">
+//       <div className="mt-4 flex justify-center">
+//         <img
+//           src="./image/a.png"
+//           alt="image"
+//           className="h-24 w-[100px] rounded-[50px]"
+//         />
+//       </div>
+//       <span className="mt-4 block flex justify-center">User_1081</span>
+//       {/* <span className="mt-2 block flex justify-center text-[#9ca3af]">
+//         @asil0312
+//       </span> */}
+//       <span className="mt-2 block flex justify-center text-[#9ca3af]">
+//         Data Scientist
+//       </span>
+//       <span className="mt-2 flex pl-4 pr-4 text-center text-sm text-[#B2CCD5]">
+//         To extract insights from data, use techniques like machine learning
+//         and statistical analysis.
+//       </span>
+//     </div>
+
+//
 import React from "react";
-import image from "biyani.jpg";
+
+// Array of colors
+const arr = ["bg-orange-200", "bg-yellow-400", "bg-red-300"];
+
+// Helper function
+
+function getInitialWithBgColor(username: string) {
+  const firstLetter = username.charAt(0).toUpperCase();
+  const randomBgColor = arr[Math.floor(Math.random() * arr.length)];
+  return { firstLetter, randomBgColor };
+}
+
 const Community = () => {
+  const users = [
+    {
+      username: "User_1081",
+      role: "Data Scientist",
+      description:
+        "To extract insights from data, use techniques like machine learning and statistical analysis.",
+    },
+    {
+      username: "Abdullah_123",
+      role: "Cybersecurity Analyst",
+      description:
+        "To protect systems from cyber threats, use security tools and best practices.",
+    },
+    {
+      username: "sam_021",
+      role: "Software Engineer",
+      description:
+        "To develop software solutions, use programming languages like Python and Java.",
+    },
+    {
+      username: "khubab_2345",
+      role: "Database Administrator",
+      description: "To manage databases, use SQL and NoSQL technologies.",
+    },
+    {
+      username: "Zoro_2352",
+      role: "UI/UX Designer",
+      description:
+        "To create user-friendly interfaces, use design tools like Figma and Adobe XD.",
+    },
+    {
+      username: "Luffy_6777",
+      role: "Network Engineer",
+      description:
+        "To design and maintain networks, use protocols like TCP/IP and routing protocols.",
+    },
+    {
+      username: "Esdeath_7654",
+      role: "DevOps Engineer",
+      description:
+        "To automate software delivery, use tools like Jenkins and Docker.",
+    },
+    {
+      username: "Albedo_6485",
+      role: "Game Designer",
+      description:
+        "To create immersive game worlds, use game engines like Unity and Unreal Engine.",
+    },
+    {
+      username: "Makami_2844",
+      role: "Software Developer",
+      description:
+        "To center, use display: flex, justify-content: center, and align-items: center.",
+    },
+  ];
+
   return (
-    <div className="scrollTop:100 bg-[#020817] pb-20">
-      <div className="">
+    <div className="bg-[#020817]">
+      <div>
         <div className="mb-[80px] ml-14 mt-32 text-[25px] font-bold uppercase">
           All Users
         </div>
       </div>
 
       <div className="ml-6 mr-6 grid gap-4 sm:grid-cols-1 lg:grid-cols-3">
-        <div className="h-[290px] w-full rounded-[12px] border-2 border-[#1e293b] bg-[#151821]">
-          <div className="mt-4 flex justify-center">
-            <img
-              src="./image/a.png"
-              alt="image"
-              className="h-24 w-[100px] rounded-[50px]"
-            />
-          </div>
-          <span className="mt-4 block flex justify-center">User_1081</span>
-          {/* <span className="mt-2 block flex justify-center text-[#9ca3af]">
-            @asil0312
-          </span> */}
-          <span className="mt-2 block flex justify-center text-[#9ca3af]">
-            Data Scientist
-          </span>
-          <span className="mt-2 flex pl-4 pr-4 text-center text-sm text-[#B2CCD5]">
-            To extract insights from data, use techniques like machine learning
-            and statistical analysis.
-          </span>
-        </div>
+        {users.map((user, index) => {
+          const { firstLetter, randomBgColor } = getInitialWithBgColor(
+            user.username,
+          );
 
-        <div className="h-[290px] w-full rounded-[12px] border-2 border-[#1e293b] bg-[#151821]">
-          <div className="mt-4 flex justify-center">
-            <img
-              src="./image/b.png"
-              alt="image"
-              className="h-24 w-[100px] rounded-[50px]"
-            />
-          </div>
-          <span className="mt-4 block flex justify-center">User_574</span>
-          <span className="mt-2block flex justify-center text-[#9ca3af]">
-            Cybersecurity Analyst
-          </span>
-          <span className="mt-4 flex pl-4 pr-4 text-center text-sm text-[#B2CCD5]">
-            To protect systems from cyber threats, use security tools and best
-            practices.
-          </span>
-        </div>
-
-        <div className="h-[290px] w-full rounded-[12px] border-2 border-[#1e293b] bg-[#151821]">
-          <div className="mt-4 flex justify-center">
-            <img
-              src="./image/j.jpg"
-              alt="image"
-              className="h-24 w-[100px] rounded-[50px]"
-            />
-          </div>
-          <span className="mt-4 block flex justify-center">User_3736</span>
-          <span className="mt-2 block flex justify-center text-[#9ca3af]">
-            Software Engineer
-          </span>
-          <span className="mt-4 flex pl-4 pr-4 text-center text-sm text-[#B2CCD5]">
-            To develop software solutions, use programming languages like Python
-            and Java.
-          </span>
-        </div>
-
-        <div className="h-[290px] w-full rounded-[12px] border-2 border-[#1e293b] bg-[#151821]">
-          <div className="mt-4 flex justify-center">
-            <img
-              src="./image/s.png"
-              alt="image"
-              className="h-24 w-[100px] rounded-[50px]"
-            />
-          </div>
-          <span className="mt-4 block flex justify-center">User_2345</span>
-          <span className="mt-2 block flex justify-center text-[#9ca3af]">
-            Database Administrator
-          </span>
-          <span className="mt-4 flex pl-4 pr-4 text-center text-sm text-[#B2CCD5]">
-            To manage databases, use SQL and NoSQL technologies.
-          </span>
-        </div>
-
-        <div className="h-[290px] w-full rounded-[12px] border-2 border-[#1e293b] bg-[#151821]">
-          <div className="mt-4 flex justify-center">
-            <img
-              src="./image/f.png"
-              alt="image"
-              className="h-24 w-[100px] rounded-[50px]"
-            />
-          </div>
-          <span className="mt-4 block flex justify-center">User_2352</span>
-          <span className="mt-2 block flex justify-center text-[#9ca3af]">
-            UI/UX Designer
-          </span>
-          <span className="mt-4 flex pl-4 pr-4 text-center text-sm text-[#B2CCD5]">
-            To create user-friendly interfaces, use design tools like Figma and
-            Adobe XD.
-          </span>
-        </div>
-
-        <div className="h-[290px] w-full rounded-[12px] border-2 border-[#1e293b] bg-[#151821]">
-          <div className="mt-4 flex justify-center">
-            <img
-              src="./image/r.jpg"
-              alt="image"
-              className="h-24 w-[100px] rounded-[50px]"
-            />
-          </div>
-          <span className="mt-4 block flex justify-center">User_6777</span>
-          <span className="mt-2 block flex justify-center text-[#9ca3af]">
-            Network Engineer
-          </span>
-          <span className="mt-4 flex pl-4 pr-4 text-center text-sm text-[#B2CCD5]">
-            To design and maintain networks, use protocols like TCP/IP and
-            routing protocols.
-          </span>
-        </div>
-
-        <div className="h-[290px] w-full rounded-[12px] border-2 border-[#1e293b] bg-[#151821]">
-          <div className="mt-4 flex justify-center">
-            <img
-              src="./image/h.png"
-              alt="image"
-              className="h-24 w-[100px] rounded-[50px]"
-            />
-          </div>
-          <span className="mt-4 block flex justify-center">User_7654</span>
-          <span className="mt-2 block flex justify-center text-[#9ca3af]">
-            DevOps Engineer
-          </span>
-          <span className="mt-4 flex pl-4 pr-4 text-center text-sm text-[#B2CCD5]">
-            To automate software delivery, use tools like Jenkins and Docker.
-          </span>
-        </div>
-
-        <div className="h-[290px] w-full rounded-[12px] border-2 border-[#1e293b] bg-[#151821]">
-          <div className="mt-4 flex justify-center">
-            <img
-              src="./image/g.png"
-              alt="image"
-              className="h-24 w-[100px] rounded-[50px]"
-            />
-          </div>
-          <span className="mt-4 block flex justify-center">User_6485</span>
-          <span className="mt-2 block flex justify-center text-[#9ca3af]">
-            Game Designer
-          </span>
-          <span className="mt-4 flex pl-4 pr-4 text-center text-sm text-[#B2CCD5]">
-            To create immersive game worlds, use game engines like Unity and
-            Unreal Engine.
-          </span>
-        </div>
-
-        <div className="h-[290px] w-full rounded-[12px] border-2 border-[#1e293b] bg-[#151821]">
-          <div className="mt-4 flex justify-center">
-            <img
-              src="./image/b.png"
-              alt="image"
-              className="h-24 w-[100px] rounded-[50px]"
-            />
-          </div>
-          <span className="mt-4 block flex justify-center">User_2844</span>
-          {/* <span className="mt-2 block flex justify-center text-[#9ca3af]">
-            @asil0312
-          </span> */}
-          <span className="mt-2 block flex justify-center text-[#9ca3af]">
-            Software Developer
-          </span>
-          <span className="mt-4 flex pl-4 pr-4 text-center text-sm text-[#B2CCD5]">
-            To center, use display: flex, justify-content: center, and
-            align-items: center.
-          </span>
-        </div>
+          return (
+            <div
+              key={index}
+              className="h-[290px] w-full rounded-[12px] border-2 border-[#1e293b] bg-[#151821]"
+            >
+              <div className="mt-4 flex justify-center">
+                <div
+                  className={`${randomBgColor} flex h-24 w-[100px] items-center justify-center rounded-full text-3xl font-semibold text-white`}
+                >
+                  {firstLetter}
+                </div>
+              </div>
+              <span className="mt-4 block flex justify-center">
+                {user.username}
+              </span>
+              <span className="mt-2 block flex justify-center text-[#9ca3af]">
+                {user.role}
+              </span>
+              <span className="mt-4 flex pl-4 pr-4 text-center text-sm text-[#B2CCD5]">
+                {user.description}
+              </span>
+            </div>
+          );
+        })}
       </div>
       <div className="mt-12 flex justify-center">
         <button className="h-10 w-14 rounded-[4px] border-none bg-gray-700 text-black">
