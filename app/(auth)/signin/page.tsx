9bcb35246fccf1ca173a8793d5cde5968eab60b1
Abdullah <abdullah95300007@gmail.com>
2024-11-19 16:09:45 +0500

    started signin

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Image from "next/image";
+import logo from "../../../Images/logo.png";
 
 const Signin = () => {
-  return <div>Signin</div>;
+  return (
+    <div className="bg-[#020817]">
+      heh hello
+      <div>
+        <ul>
+          <li>
+            <span>
+              <Image
+                src={logo}
+                alt="logo"
+                className="absolute top-5 h-12 w-32 rounded-md bg-white hover:cursor-pointer dark:bg-orange-500"
+              />
+            </span>
+          </li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Signin;
